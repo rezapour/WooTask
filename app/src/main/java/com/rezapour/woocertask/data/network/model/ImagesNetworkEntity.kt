537d96dc2e38513c5c8a30a9b503d9a5f6ea3947
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-12-12 03:11:24 +0330

    patch:(edit small things)

--- app/src/main/java/com/rezapour/woocertask/data/network/model/ImagesNetworkEntity.kt
+++ app/src/main/java/com/rezapour/woocertask/data/network/model/ImagesNetworkEntity.kt
@@ -3,10 +3,10 @@ import com.google.gson.annotations.SerializedName
 data class ImagesNetworkEntity (
 
 	@SerializedName("id") val id : Int,
-	@SerializedName("date_created") val date_created : String,
-	@SerializedName("date_created_gmt") val date_created_gmt : String,
-	@SerializedName("date_modified") val date_modified : String,
-	@SerializedName("date_modified_gmt") val date_modified_gmt : String,
+	@SerializedName("date_created") val dateCreated : String,
+	@SerializedName("date_created_gmt") val dateCreatedGmt : String,
+	@SerializedName("date_modified") val dateModified : String,
+	@SerializedName("date_modified_gmt") val dateModifiedGmt : String,
 	@SerializedName("src") val src : String,
 	@SerializedName("name") val name : String,
 	@SerializedName("alt") val alt : String
--- app/src/main/java/com/rezapour/woocertask/data/network/model/SelfNetworkEntity.kt
+++ app/src/main/java/com/rezapour/woocertask/data/network/model/SelfNetworkEntity.kt
@@ -1,6 +1,5 @@
 package com.rezapour.woocertask.data.network.model
 import com.google.gson.annotations.SerializedName
 data class SelfNetworkEntity (
-
 	@SerializedName("href") val href : String
 )
