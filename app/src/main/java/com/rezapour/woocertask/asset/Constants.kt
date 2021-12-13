1e05f3efcec9a71becb2028624de3a6de9bc7e2f
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2021-12-13 06:17:47 +0330

    patch:(clean code)

--- app/src/main/java/com/rezapour/woocertask/asset/Constants.kt
+++ app/src/main/java/com/rezapour/woocertask/asset/Constants.kt
@@ -4,6 +4,5 @@ object Constants {
     const val BASE_URL = "https://wpt.woocer.com/"
     const val TIME_OUT: Long = 5000
     const val DATA_BASE_NAME = "app_db"
-    const val Ck = "ck_85f212310cfff32728cc4c933331aa6bcf3002ef"
-    const val CS = "cs_ee784168289012a919a008985d2252fadecea2bb"
+    const val SUB_DOMAIN="/wp-json/wc/v3/products"
 }
